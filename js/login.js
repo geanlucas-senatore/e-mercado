c6af020d12b7db9bfa7c1a915f118033fa78054a
geanlucas-senatore <lucassenatore996@gmail.com>
2020-08-09 18:25:16 -0300

    login creado, products y redireccion

--- js/login.js
+++ js/login.js
@@ -1,6 +1,29 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
+
 
 });
+
+function funcionamiento() {
+    let guardar = localStorage.setItem("correo", document.getElementById("email").value);
+    let guardar2 = localStorage.setItem("contra", document.getElementById("contra").value);
+
+}
+let codigo = localStorage.getItem("correo");
+let codigo2 = localStorage.getItem("contra");
+document.getElementById("hola").innerHTML = codigo + " " + codigo2;
+if (codigo == null) {
+    if (codigo2 == null) {
+        window.location.replace("login.html");
+    }
+}
+if (codigo && !codigo2 || !codigo && codigo2) {
+    window.location.replace("login.html");
+}
+
+function borrar() {
+    localStorage.removeItem("correo");
+    localStorage.removeItem("contra");
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,43 @@
+var categoriesArray = [];
+
+function showCategoriesList(array) {
+
+    let contenidoproductos = "";
+    for (let i = 0; i < array.length; i++) {
+        let category = array[i];
+
+        contenidoproductos += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">` + category.name + `</h4>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                    </div>
+                    <h6 class="mb-1">` + category.description + `</h6><br>
+                    <h6 class="mb-1">` + category.cost + category.currency + `</h6>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("contenedor").innerHTML = contenidoproductos;
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e) {
+    showSpinner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+            hideSpinner();
+        }
+    });
 });
