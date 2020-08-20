bc5ccf898ec397178fa67daeb9068f2e85f23c6c
geanlucas-senatore <lucassenatore996@gmail.com>
2020-08-20 15:34:03 -0300

    orden de productos por precio y ventas

--- js/products.js
+++ js/products.js
@@ -1,31 +1,86 @@
-var categoriesArray = [];
+const ORDER_ASC_BY_PRICE = "$MAX";
+const ORDER_DESC_BY_PRICE = "$MIN";
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_BY_SOLD_COUNT = "Vend."
+currentArray = [];
+currentcomordenarlos = undefined;
+minCount = undefined;
+maxCount = undefined;
 
-function showCategoriesList(array) {
+function ordenar(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function(a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_ASC_BY_PRICE) {
+        result = array.sort(function(a, b) {
+            return a.cost - b.cost;
+        });
+    } else if (criteria === ORDER_DESC_BY_PRICE) {
+        result = array.sort(function(a, b) {
+            return b.cost - a.cost;
+        });
+    } else if (criteria === ORDER_BY_SOLD_COUNT) {
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
 
-    let contenidoproductos = "";
-    for (let i = 0; i < array.length; i++) {
-        let category = array[i];
-
-        contenidoproductos += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="` + category.desc + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">` + category.name + `</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
-                    </div>
-                    <h6 class="mb-1">` + category.description + `</h6><br>
-                    <h6 class="mb-1">` + category.cost + category.currency + `</h6>
-                </div>
-            </div>
-        </div>
-        `
-
-        document.getElementById("contenedor").innerHTML = contenidoproductos;
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+        });
     }
+
+    return result;
+}
+
+function mostrarproductos() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentArray.length; i++) {
+        let productos = currentArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(productos.soldCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(productos.soldCount) <= maxCount))) {
+
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + productos.imgSrc + `" alt="` + productos.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h3 class="mb-1">` + productos.name + `</h3>
+                            <h5 class="text-muted">` + productos.soldCount + ` artículos</h5>
+                        </div>
+                        <h4 class="mb-1">` + productos.description + `</h4>
+                        <br><br><br>
+                        <h4 class="mb-1">Precio:&nbsp` + productos.cost + productos.currency + `</h4>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("contenedor").innerHTML = htmlContentToAppend;
+    }
+}
+
+function ordenarymostrarcategorias(comordenarlos, array) {
+    currentcomordenarlos = comordenarlos;
+
+    if (array != undefined) {
+        currentArray = array;
+    }
+
+    currentArray = ordenar(currentcomordenarlos, currentArray);
+
+    //Muestro las categorías ordenadas
+    mostrarproductos();
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -34,10 +89,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
     showSpinner();
     getJSONData(PRODUCTS_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
-            categoriesArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
-            hideSpinner();
+            ordenarymostrarcategorias(ORDER_ASC_BY_NAME, resultObj.data);
         }
     });
+    document.getElementById("sortporpreciomin").addEventListener("click", function() {
+        ordenarymostrarcategorias(ORDER_ASC_BY_PRICE);
+    });
+    document.getElementById("sortporpreciomax").addEventListener("click", function() {
+        ordenarymostrarcategorias(ORDER_DESC_BY_PRICE);
+    });
+    document.getElementById("sortporventas").addEventListener("click", function() {
+        ordenarymostrarcategorias(ORDER_BY_SOLD_COUNT);
+    });
+
 });
