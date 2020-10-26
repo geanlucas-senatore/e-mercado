993ab678fd4b9ceaa294ace537b9fd8499a7f4db
geanlucas-senatore <lucassenatore996@gmail.com>
2020-10-26 18:43:19 -0300

    funcionamiento de guardar datos  de usuarios lista

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,4 +1,47 @@
 let informa = [];
+let nombreGuardado = localStorage.getItem("correo");
+
+
+function mostrarInformacion() {
+
+    infoGuardada = localStorage.getItem(nombreGuardado);
+
+    if (infoGuardada !== null) {
+
+
+        infoParseada = JSON.parse(infoGuardada);
+
+
+        for (i = 0; i < infoParseada.length; i++) {
+
+            let info = infoParseada[i];
+
+
+            document.getElementById("nombretomado").value = info.nombre;
+            document.getElementById("edadtomada").value = info.edad;
+            document.getElementById("emailtomado").value = info.email;
+            document.getElementById("direcciontomada").value = info.direccion;
+
+        }
+    }
+}
+
+form.onsubmit = function(evento) {
+
+    evento.preventDefault();
+
+    let nombreToma = document.getElementById("nombretomado").value;
+    let edadToma = document.getElementById("edadtomada").value;
+    let mailToma = document.getElementById("emailtomado").value;
+    let dirToma = document.getElementById("direcciontomada").value;
+
+    let guardarJson = JSON.stringify([{ usuario: nombreGuardado, nombre: nombreToma, edad: edadToma, email: mailToma, direccion: dirToma }]);
+
+    localStorage.setItem(nombreGuardado, guardarJson);
+
+}
+
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -14,6 +57,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
             document.getElementById("alerta").innerHTML = informa["articles"].length;
         }
     });
-
-
+    mostrarInformacion();
+    //localStorage.removeItem("informacion");
 });
